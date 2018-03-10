fb423200b7f4503eb4badf79bfdb101b5d714b05
Timothy Pan <>
2018-03-10 15:41:25 -0500

    redux dev tools:

--- app/javascript/bundles/ImageCategorization/startup/ImageCategorizationApp.jsx
+++ app/javascript/bundles/ImageCategorization/startup/ImageCategorizationApp.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import configureStore from '../store/imageCategorizationStore';
+import getStore from '../store/imageCategorizationStore';
 import ImageCategorizationContainer from '../containers/ImageCategorizationContainer';
 
 const ImageCategorizationApp = (props) => (
-  <Provider store={configureStore(props)}>
+  <Provider store={getStore(props.dev_environment)}>
     <ImageCategorizationContainer />
   </Provider>
 );
--- app/javascript/bundles/ImageCategorization/store/imageCategorizationStore.js
+++ app/javascript/bundles/ImageCategorization/store/imageCategorizationStore.js
@@ -1,8 +1,15 @@
 import { createStore } from 'redux';
 import imageCategorizationReducer from '../reducers/imageCategorizationReducer';
 
-const configureStore = (railsProps) => (
-  createStore(imageCategorizationReducer, railsProps)
-);
+export default function getStore(dev) {
+  const store = dev && window.__REDUX_DEVTOOLS_EXTENSION__
+    ? createStore(imageCategorizationReducer, window.__REDUX_DEVTOOLS_EXTENSION__())
+    : createStore(imageCategorizationReducer);
 
-export default configureStore;
+  // const sagaMiddleware = createSagaMiddleware();
+  // const store = dev && window.__REDUX_DEVTOOLS_EXTENSION__
+  //   ? createStore(rootReducer, compose(applyMiddleware(sagaMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__()))
+  //   : createStore(rootReducer, applyMiddleware(sagaMiddleware));
+  // sagaMiddleware.run(rootSaga);
+  return store;
+}
