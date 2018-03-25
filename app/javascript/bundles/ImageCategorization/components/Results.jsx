1174dc823f4b1b5a14f15e21b4f0284ca00bcf63
Timothy Pan <>
2018-03-24 20:52:03 -0400

    Write to dropbox

--- app/javascript/bundles/ImageCategorization/components/Results.jsx
+++ app/javascript/bundles/ImageCategorization/components/Results.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { KEY_TO_CATEGORY_NAME } from '../constants/imageCategorizationConstants';
+import { Dropbox } from 'dropbox';
+import { v4 } from 'uuid';
+
+const ACCESS_TOKEN = 'LwE7g1TIELAAAAAAAAAACEbfzIGvug0V2shO18EWsm7vLkMXLrbn4lRRw1ikaqmw';
+const dbx = new Dropbox({ accessToken: ACCESS_TOKEN });
+
+//score
+//time
+//results dump
+//confirmation or no
+
+function test() {
+  dbx.filesUpload({
+    path: '/CSC428/test.txt',
+    contents: 'Text Content3',
+    mode: { ".tag": "overwrite" },
+    autorename: false
+  }).then((data) => { console.log(data); })
+    .catch((error) => { console.error(error); })
+}
 
 function generateRows(results) {
-  return results.map((category, imgIndex) => (<tr key={imgIndex}><td>{imgIndex}</td><td>{KEY_TO_CATEGORY_NAME[category]}</td></tr>))
-    .toArray();
+  return results.map(
+    (category, imgIndex) => (<tr key={imgIndex}><td>{imgIndex}</td><td>{KEY_TO_CATEGORY_NAME[category]}</td></tr>)
+  ).toArray();
 }
 
 function generateResultsTable(results) {
@@ -26,7 +47,7 @@ function Results(props) {
     <div id="results">
       <h2>Categorization Completed</h2>
       <div>Thank you for your participation</div>
-      <button id="submit">Submit Results</button>
+      <button id="submit" onClick={test}>Submit Results</button>
       {generateResultsTable(props.results)}
     </div>
   );
